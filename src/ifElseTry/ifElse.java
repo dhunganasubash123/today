89d38a801bb5d9ae74fc1610ac80a37b6af96f6e
subashdhungana123 <92055996+subashdhungana123@users.noreply.github.com>
2021-11-11 15:20:54 -0600

    my first commit

---
+++ src/ifElseTry/ifElse.java
@@ -0,0 +1,23 @@
+package ifElseTry;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		int day = 3;
+		switch (day) {
+		case 1:
+			System.out.println("Today is Sunday");
+			break;
+		case 2:
+			System.out.println("Today is Monday");
+			break;
+		case 4:
+			System.out.println("Today is Wednesday");
+			break;
+			default:
+				System.out.println("Today is Off");
+			
+		}
+	}
+	}
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ifElse {
+}
